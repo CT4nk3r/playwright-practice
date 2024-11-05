88b3739eb920a87849bdfc6b624e37497452b763
CT4nk3r <bosze.mate1997@gmail.com>
2024-11-05 13:41:55 +0100

    changed to locators

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -6,28 +6,28 @@ test.describe('testing login', () => {
   })
   
   test('test successful login', async ({ page }) => {
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('practice');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('SuperSecretPassword!');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#username').click();
+    await page.locator('#username').fill('practice');
+    await page.locator('#password').click();
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('.btn-primary').click();
     expect(page.locator('#flash').filter({ hasText: 'You logged into a secure area!' })).toBeTruthy();
   });
 
   test('test invalid username', async ({ page }) => {
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('asd');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('SuperSecretPassword!');
-    await page.getByRole('button', { name: 'Login' }).click();
+    await page.locator('#username').click();
+    await page.locator('#username').fill('asd');
+    await page.locator('#password').click();
+    await page.locator('#password').fill('SuperSecretPassword!');
+    await page.locator('.btn-primary').click();
     expect(page.locator('#flash').filter({ hasText: 'Your username is invalid!' })).toBeTruthy();
   })
 
   test('test invalid password', async ({ page }) => {
-    await page.getByLabel('Username').click();
-    await page.getByLabel('Username').fill('practice');
-    await page.getByLabel('Password').click();
-    await page.getByLabel('Password').fill('asd');
+    await page.locator('#username').click();
+    await page.locator('#username').fill('practice');
+    await page.locator('#password').click();
+    await page.locator('#password').fill('asd');
     await page.getByRole('button', { name: 'Login' }).click();
     expect(page.locator('#flash').filter({ hasText: 'Your password is invalid!' })).toBeTruthy();
   })
