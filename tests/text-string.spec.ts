a34beb55e8557cb721947ba49d705514518885b1
CT4nk3r <bosze.mate1997@gmail.com>
2024-11-06 14:26:28 +0100

    expect *fill* instead of the whole sentence

--- tests/text-string.spec.ts
+++ tests/text-string.spec.ts
@@ -13,7 +13,7 @@ test.describe('testing text input', () => {
             const input = element as HTMLInputElement
             return input.validationMessage
         })
-        expect(validationMessage).toContain('Please fill in this field.');
+        expect(validationMessage).toContain('*fill*');
     })
     
     test('with less than 5 characters', async ({ page }) => {
